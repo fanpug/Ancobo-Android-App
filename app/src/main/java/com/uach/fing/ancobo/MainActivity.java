5aa2447d1c89108c612e7e024bb8dd1c667467cb
Humberto Navarro <a320669@uach.mx>
2021-05-24 11:55:16 -0600

    Se agregan dependencias necesarias y permiso a internet

--- app/src/main/java/com/uach/fing/ancobo/MainActivity.java
+++ app/src/main/java/com/uach/fing/ancobo/MainActivity.java
@@ -2,7 +2,24 @@ package com.uach.fing.ancobo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+import com.uach.fing.weather.models.Weather;
+
+import android.content.Context;
+import android.graphics.ColorSpace;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 
 public class MainActivity extends AppCompatActivity {
 
