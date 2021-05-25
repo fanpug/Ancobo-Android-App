8ebc138b6abe1266f08bdcb8184e7f7f5bd90869
Humberto Navarro <a320669@uach.mx>
2021-05-25 00:50:20 -0600

    Se agrega la estructura para introducir informacion acorde a la persona

--- app/src/main/java/com/uach/fing/ancobo/Curriculum.java
+++ app/src/main/java/com/uach/fing/ancobo/Curriculum.java
@@ -26,11 +26,30 @@ public class Curriculum extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String identifier = getIntent().getStringExtra("identifier");
+        int identifier = Integer.parseInt(getIntent().getStringExtra("identifier"));
         setContentView(R.layout.activity_curriculum);
-        TextView txt_name = findViewById(R.id.txt_name);
 
-        txt_name.setText("Default Namee");
+        //Variables para cambiar el texto del curriculum
+        TextView txt_name = findViewById(R.id.txt_name);
+        TextView txt_profession = findViewById(R.id.txt_profession);
+        TextView txt_about_me_description = findViewById(R.id.txt_about_me_description);
+        TextView txt_education_description = findViewById(R.id.txt_education_description);
+        TextView txt_skills_description = findViewById(R.id.txt_skills_description);
+
+        //Switch para cambiar la informacion acorde al boton
+        switch(identifier){
+            case R.id.btn_humberto:
+
+                break;
+
+            case R.id.btn_sergio:
+
+                break;
+
+            case R.id.btn_kevin:
+
+                break;
+        }
 
     }
 }
